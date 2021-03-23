5a77bc9aec0f167a6d773721e98efa34ae52c2c0
Spartak Ryabtsev <spartak.r@gmail.com>
2021-03-23 04:05:43 +0300

    cleanup

--- src/FileService.Models.Dto/Models/File.cs
+++ src/FileService.Models.Dto/Models/File.cs
@@ -1,11 +1,9 @@
 using System;
-using System.ComponentModel.DataAnnotations;
 
 namespace LT.DigitalOffice.FileService.Models.Dto.Models
 {
     public class File
     {
-        [Key]
         public Guid? Id { get; set; }
         public string Content { get; set; }
         public string Extension { get; set; }
