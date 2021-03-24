ff78397b3f3f4f6b9f3810c75eb73c38367072f3
KostinP <>
2021-03-24 17:15:58 +0300

    Added assembly attribute

--- src/FileService.Business/Helpers/ImageToSquareAlgorithm.cs
+++ src/FileService.Business/Helpers/ImageToSquareAlgorithm.cs
@@ -10,7 +10,9 @@ using SixLabors.ImageSharp.Formats.Tga;
 using SixLabors.ImageSharp.Processing;
 using System;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 
+[assembly: InternalsVisibleTo("LT.DigitalOffice.FileService")]
 namespace LT.DigitalOffice.FileService.Business.Helpers
 {
     internal class ImageToSquareAlgorithm : IImageResizeAlgorithm
