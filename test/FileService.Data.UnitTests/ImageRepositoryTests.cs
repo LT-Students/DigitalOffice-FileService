5cda8e58de45cfb119eabc089fd1f75468e5bafb
BelehovEgor <belehov.egor2001@yandex.ru>
2021-07-22 10:56:48 +0300

    added test

--- test/FileService.Data.UnitTests/ImageRepositoryTests.cs
+++ test/FileService.Data.UnitTests/ImageRepositoryTests.cs
@@ -6,6 +6,7 @@ using LT.DigitalOffice.UnitTestKernel;
 using Microsoft.EntityFrameworkCore;
 using NUnit.Framework;
 using System;
+using System.Collections.Generic;
 
 namespace LT.DigitalOffice.FileService.Data.UnitTests
 {
@@ -93,6 +94,46 @@ namespace LT.DigitalOffice.FileService.Data.UnitTests
             _dbContext.SaveChanges();
 
             SerializerAssert.AreEqual(image, _repository.Get(image.Id));
+        }
+
+        [Test]
+        public void ShouldGetImagesByIdsSuccessfuly()
+        {
+            DbImage image1 = new()
+            {
+                Id = Guid.NewGuid(),
+                ParentId = Guid.NewGuid(),
+                Name = "name",
+                Content = "content",
+                Extension = "extansion",
+                AddedOn = DateTime.UtcNow,
+                IsActive = true,
+                ImageType = 0,
+                UserId = Guid.NewGuid()
+            };
+
+            DbImage image2 = new()
+            {
+                Id = Guid.NewGuid(),
+                ParentId = Guid.NewGuid(),
+                Name = "name",
+                Content = "content",
+                Extension = "extansion",
+                AddedOn = DateTime.UtcNow,
+                IsActive = true,
+                ImageType = 0,
+                UserId = Guid.NewGuid()
+            };
+
+            _dbContext.Images.Add(image1);
+            _dbContext.Images.Add(image2);
+            _dbContext.SaveChanges();
+
+            List<DbImage> images = _repository.Get(new List<Guid> { image1.Id, image2.Id });
+
+            Assert.IsTrue(images.Contains(image1));
+            Assert.IsTrue(images.Contains(image2));
+            Assert.IsTrue(images.Count == 2);
         }
 
         #endregion
