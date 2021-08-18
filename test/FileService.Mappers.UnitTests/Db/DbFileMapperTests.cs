e7fd6e10dff27ad5380d6e8e34f3ae041e0ebde3
VLiamin <LyaminVA@yandex.ru>
2021-08-18 17:59:49 +0300

    fixed tests

--- test/FileService.Mappers.UnitTests/Db/DbFileMapperTests.cs
+++ test/FileService.Mappers.UnitTests/Db/DbFileMapperTests.cs
@@ -2,23 +2,39 @@
 using LT.DigitalOffice.FileService.Mappers.Db.Interfaces;
 using LT.DigitalOffice.FileService.Models.Db;
 using LT.DigitalOffice.FileService.Models.Dto.Requests;
+using LT.DigitalOffice.Kernel.Extensions;
 using LT.DigitalOffice.UnitTestKernel;
+using Microsoft.AspNetCore.Http;
+using Moq.AutoMock;
 using NUnit.Framework;
 using System;
+using System.Collections.Generic;
 
 namespace LT.DigitalOffice.FileService.Mappers.UnitTests.Db
 {
     public class DbFileMapperTests
     {
+        private AutoMocker _autoMocker;
         private IDbFileMapper _dbFileMapper;
-
+        private IDictionary<object, object> _items;
         private DbFile _dbFile;
         private AddFileRequest _fileRequest;
+        private Guid _userId;
 
         [SetUp]
         public void SetUp()
         {
-            _dbFileMapper = new DbFileMapper();
+            _autoMocker = new();
+
+            _userId = Guid.NewGuid();
+            _items = new Dictionary<object, object>();
+            _items.Add("UserId", _userId);
+
+            _dbFileMapper = _autoMocker.CreateInstance<DbFileMapper>();
+
+            _autoMocker
+               .Setup<IHttpContextAccessor, IDictionary<object, object>>(x => x.HttpContext.Items)
+               .Returns(_items);
 
             _fileRequest = new AddFileRequest
             {
@@ -57,7 +73,8 @@ namespace LT.DigitalOffice.FileService.Mappers.UnitTests.Db
                 Extension = _fileRequest.Extension,
                 Name = _fileRequest.Name,
                 IsActive = true,
-                CreatedAtUtc = newFile.CreatedAtUtc
+                CreatedAtUtc = newFile.CreatedAtUtc,
+                CreatedBy = _userId
             };
 
             SerializerAssert.AreEqual(expectedFile, newFile);
