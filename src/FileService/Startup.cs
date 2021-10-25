72a7128aa3df9a5bb3cbee25f8d746bdb250ea56
VLiamin <LyaminVA@yandex.ru>
2021-10-25 13:02:28 +0300

    update version

--- src/FileService/Startup.cs
+++ src/FileService/Startup.cs
@@ -42,7 +42,7 @@ namespace LT.DigitalOffice.FileService
                 .GetSection(BaseRabbitMqConfig.SectionName)
                 .Get<RabbitMqConfig>();
 
-            Version = "1.2.4";
+            Version = "1.1.1.0";
             Description = "FileService is an API intended to work with files and images.";
             StartTime = DateTime.UtcNow;
             ApiName = $"LT Digital Office - {_serviceInfoConfig.Name}";
