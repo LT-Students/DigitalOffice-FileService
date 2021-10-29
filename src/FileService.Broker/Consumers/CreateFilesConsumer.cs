e1d721ad651f912e39c0202bd10dd5710297b3fc
VLiamin <LyaminVA@yandex.ru>
2021-10-29 03:30:11 +0300

    added editorconfig

--- src/FileService.Broker/Consumers/CreateFilesConsumer.cs
+++ src/FileService.Broker/Consumers/CreateFilesConsumer.cs
@@ -1,37 +1,36 @@
-﻿using LT.DigitalOffice.FileService.Data.Interfaces;
+﻿using System.Linq;
+using System.Threading.Tasks;
+using LT.DigitalOffice.FileService.Data.Interfaces;
 using LT.DigitalOffice.FileService.Mappers.Db.Interfaces;
 using LT.DigitalOffice.Kernel.Broker;
 using LT.DigitalOffice.Models.Broker.Requests.File;
 using MassTransit;
-using System;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace LT.DigitalOffice.FileService.Broker.Consumers
 {
-    public class CreateFilesConsumer : IConsumer<ICreateFilesRequest>
+  public class CreateFilesConsumer : IConsumer<ICreateFilesRequest>
+  {
+    private readonly IFileRepository _repository;
+    private readonly IDbFileMapper _mapper;
+
+    private async Task<bool> CreateFilesAsync(ICreateFilesRequest request)
     {
-        private readonly IFileRepository _repository;
-        private readonly IDbFileMapper _mapper;
-
-        private async Task<bool> CreateFilesAsync(ICreateFilesRequest request)
-        {
-            return await _repository.CreateAsync(request.Files.Select(x => _mapper.Map(x, request.CreatedBy)).ToList());
-        }
-
-        public CreateFilesConsumer(
-            IFileRepository repository,
-            IDbFileMapper mapper)
-        {
-            _repository = repository;
-            _mapper = mapper;
-        }
-
-        public async Task Consume(ConsumeContext<ICreateFilesRequest> context)
-        {
-            object response = OperationResultWrapper.CreateResponse(CreateFilesAsync, context.Message);
-
-            await context.RespondAsync<IOperationResult<bool>>(response);
-        }
+      return await _repository.CreateAsync(request.Files.Select(x => _mapper.Map(x, request.CreatedBy)).ToList());
     }
+
+    public CreateFilesConsumer(
+      IFileRepository repository,
+      IDbFileMapper mapper)
+    {
+      _repository = repository;
+      _mapper = mapper;
+    }
+
+    public async Task Consume(ConsumeContext<ICreateFilesRequest> context)
+    {
+      object response = OperationResultWrapper.CreateResponse(CreateFilesAsync, context.Message);
+
+      await context.RespondAsync<IOperationResult<bool>>(response);
+    }
+  }
 }
--- src/FileService/Controllers/FileController.cs
+++ src/FileService/Controllers/FileController.cs
@@ -1,31 +1,30 @@
-﻿using LT.DigitalOffice.FileService.Business.Commands.File.Interfaces;
-using LT.DigitalOffice.FileService.Models.Dto.Models;
-using LT.DigitalOffice.FileService.Models.Dto.Requests;
-using Microsoft.AspNetCore.Mvc;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using LT.DigitalOffice.FileService.Business.Commands.File.Interfaces;
+using LT.DigitalOffice.FileService.Models.Dto.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace LT.DigitalOffice.FileService.Controllers
 {
-    [ApiController]
-    [Route("[controller]")]
-    public class FileController : ControllerBase
+  [ApiController]
+  [Route("[controller]")]
+  public class FileController : ControllerBase
+  {
+    [HttpGet("get")]
+    public async Task<List<FileInfo>> GetAsync(
+      [FromServices] IGetFileCommand command,
+      [FromQuery] List<Guid> filesIds)
     {
-        [HttpGet("get")]
-        public async Task<List<FileInfo>> GetAsync(
-            [FromServices] IGetFileCommand command,
-            [FromQuery] List<Guid> filesIds)
-        {
-            return await command.Execute(filesIds);
-        }
-
-        [HttpDelete("disable")]
-        public void Disable(
-            [FromServices] IDisableFileCommand command,
-            [FromQuery] Guid fileId)
-        {
-            command.Execute(fileId);
-        }
+      return await command.Execute(filesIds);
     }
+
+    [HttpDelete("disable")]
+    public void Disable(
+      [FromServices] IDisableFileCommand command,
+      [FromQuery] Guid fileId)
+    {
+      command.Execute(fileId);
+    }
+  }
 }
