e651607710c963d96bada8746f77c8c6b8de8135
Ekaterina Ryzhenkova <vadivazikryzh@gmail.com>
2022-01-11 16:27:28 +0300

    add GetFileCommandTest

--- test/FileService.Business.UnitTests/Commands/File/GetFileCommandTests.cs
+++ test/FileService.Business.UnitTests/Commands/File/GetFileCommandTests.cs
@@ -1,4 +1,7 @@
-﻿using LT.DigitalOffice.FileService.Business.Commands.File;
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using LT.DigitalOffice.FileService.Business.Commands.File;
 using LT.DigitalOffice.FileService.Business.Commands.File.Interfaces;
 using LT.DigitalOffice.FileService.Data.Interfaces;
 using LT.DigitalOffice.FileService.Mappers.Models.Interfaces;
@@ -6,74 +9,126 @@ using LT.DigitalOffice.FileService.Models.Db;
 using LT.DigitalOffice.FileService.Models.Dto.Models;
 using LT.DigitalOffice.UnitTestKernel;
 using Moq;
+using Moq.AutoMock;
 using NUnit.Framework;
-using System;
 
 namespace LT.DigitalOffice.FileService.Business.UnitTests.Commands.File
 {
-    /*public class GetFileCommandTests
+  public class GetFileCommandTests
+  {
+    private AutoMocker _autoMocker;
+    private IGetFileCommand _command;
+    private List<DbFile> _dbFiles;
+    private List<FileInfo> _infoFile;
+    List<Guid> _ids = new() { Guid.NewGuid(), Guid.NewGuid(), Guid.NewGuid() };
+
+    [OneTimeSetUp]
+    public void OneTimeSetUp()
     {
-        private IGetFileCommand _command;
-        private Mock<IFileRepository> _repositoryMock;
-        private Mock<IFileDataMapper> _mapperMock;
+      _autoMocker = new AutoMocker();
+      _command = _autoMocker.CreateInstance<GetFileCommand>();
 
-        private DbFile _file;
-        private Guid _fileId;
-
-        [SetUp]
-        public void Setup()
+      _dbFiles = new List<DbFile>()
+      {
+        new DbFile()
         {
-            _repositoryMock = new Mock<IFileRepository>();
-            _mapperMock = new Mock<IFileDataMapper>();
-            _command = new GetFileCommand(_repositoryMock.Object, _mapperMock.Object);
+          Id = _ids[0],
+          Content = "Test1",
+          Extension = "Test1",
+          Name = "Test1",
+          CreatedAtUtc = DateTime.UtcNow,
+          CreatedBy = Guid.NewGuid(),
+        },
 
-            _fileId = Guid.NewGuid();
-            _file = new DbFile
-            {
-                Id = _fileId,
-                Name = "File",
-                Content = "RGlnaXRhbCBPZmA5Y2U=",
-                Extension = ".jpg"
-            };
-        }
-
-        [Test]
-        public void ShouldThrowExceptionWhenRepositoryThrowsIt()
+        new DbFile()
         {
-            _repositoryMock.Setup(x => x.GetFile(It.IsAny<Guid>())).Throws(new Exception());
+          Id = _ids[1],
+          Content = "Test2",
+          Extension = "Test2",
+          Name = "Test2",
+          CreatedAtUtc = DateTime.UtcNow,
+          CreatedBy = Guid.NewGuid(),
+        },
 
-            Assert.Throws<Exception>(() => _command.Execute(_fileId));
-        }
-
-        [Test]
-        public void ShouldThrowExceptionWhenMapperThrowsIt()
+        new DbFile()
         {
-            _mapperMock.Setup(x => x.Map(It.IsAny<DbFile>())).Throws(new Exception());
-
-            Assert.Throws<Exception>(() => _command.Execute(_fileId));
+          Id = _ids[2],
+          Content = "Test3",
+          Extension = "Test3",
+          Name = "Test3",
+          CreatedAtUtc = DateTime.UtcNow,
+          CreatedBy = Guid.NewGuid(),
         }
+      };
 
-        [Test]
-        public void ShouldReturnFileInfo()
-        {
-            var expected = new FileInfo
-            {
-                Id = _file.Id,
-                Name = _file.Name,
-                Content = _file.Content,
-                Extension = _file.Extension
-            };
+      _infoFile = new List<FileInfo>();
 
-            _repositoryMock
-                .Setup(x => x.GetFile(It.IsAny<Guid>()))
-                .Returns(_file);
-            _mapperMock
-                .Setup(x => x.Map(It.IsAny<DbFile>()))
-                .Returns(expected);
+      foreach (var file in _dbFiles)
+      {
+        _infoFile.Add(
+          new FileInfo()
+          {
+            Id = file.Id,
+            Content = file.Content,
+            Extension = file.Extension,
+            Name = file.Name
+          });
+      }
+    }
 
-            var result = _command.Execute(_fileId);
+    [SetUp]
+    public void SetUp()
+    {
+      _autoMocker.GetMock<IFileRepository>().Reset();
+      _autoMocker.GetMock<IFileInfoMapper>().Reset();
+    }
 
-            SerializerAssert.AreEqual(expected, result);
-        }
-    }*/
+    [Test]
+    public async Task ShouldReturnListFileInfo()
+    {
+      var result = _infoFile;
+
+      _autoMocker
+        .Setup<IFileRepository, Task<List<DbFile>>>(x => x.GetAsync(It.IsAny<List<Guid>>()))//!!!
+        .ReturnsAsync(_dbFiles);
+
+      _autoMocker
+        .SetupSequence<IFileInfoMapper, FileInfo>(x => x.Map(It.IsAny<DbFile>()))
+        .Returns(_infoFile[0])
+        .Returns(_infoFile[1])
+        .Returns(_infoFile[2]);
+
+      SerializerAssert.AreEqual(result, await _command.Execute(_ids));
+
+      _autoMocker.Verify<IFileRepository, Task<List<DbFile>>>(x => x.GetAsync(It.IsAny<List<Guid>>()), Times.Once);
+      _autoMocker.Verify<IFileInfoMapper, FileInfo>(x => x.Map(It.IsAny<DbFile>()), Times.Exactly(3));
+    }
+
+    [Test]
+    public async Task ShouldReturnNullInRepository()
+    {
+      List<DbFile> dblist = null;
+      List<Guid> ids = new() { Guid.NewGuid() };
+
+      _autoMocker
+        .Setup<IFileRepository, Task<List<DbFile>>>(x => x.GetAsync(It.IsAny<List<Guid>>()))
+        .ReturnsAsync(dblist);
+
+      SerializerAssert.AreEqual(null, await _command.Execute(ids));
+
+      _autoMocker.Verify<IFileRepository, Task<List<DbFile>>>(x => x.GetAsync(It.IsAny<List<Guid>>()), Times.Once);
+      _autoMocker.Verify<IFileInfoMapper, FileInfo>(x => x.Map(It.IsAny<DbFile>()), Times.Never);
+    }
+
+    [Test]
+    public async Task ShouldReturnNull()
+    {
+      _ids = null;
+
+      SerializerAssert.AreEqual(null, await _command.Execute(_ids));
+
+      _autoMocker.Verify<IFileRepository, Task<List<DbFile>>>(x => x.GetAsync(It.IsAny<List<Guid>>()), Times.Never);
+      _autoMocker.Verify<IFileInfoMapper, FileInfo>(x => x.Map(It.IsAny<DbFile>()), Times.Never);
+    }
+  }
 }
