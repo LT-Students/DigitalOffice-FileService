c71dc316f97af1e505fea03301b997a5b0ae61fe
VLiamin <vliamin@foss.dk>
2022-04-14 15:26:57 +0300

    added check

--- src/FileService.Business/Commands/File/GetFileCommand.cs
+++ src/FileService.Business/Commands/File/GetFileCommand.cs
@@ -6,6 +6,8 @@ using LT.DigitalOffice.FileService.Business.Commands.File.Interfaces;
 using LT.DigitalOffice.FileService.Data.Interfaces;
 using LT.DigitalOffice.FileService.Mappers.Models.Interfaces;
 using LT.DigitalOffice.FileService.Models.Dto.Models;
+using LT.DigitalOffice.Kernel.BrokerSupport.AccessValidatorEngine.Interfaces;
+using LT.DigitalOffice.Kernel.Constants;
 using LT.DigitalOffice.Kernel.Enums;
 using LT.DigitalOffice.Kernel.Responses;
 using LT.DigitalOffice.ProjectService.Broker.Requests.Interfaces;
@@ -17,15 +19,18 @@ namespace LT.DigitalOffice.FileService.Business.Commands.File
     private readonly IFileRepository _repository;
     private readonly IFileInfoMapper _mapper;
     private readonly IProjectService _projectService;
+    private readonly IAccessValidator _accessValidator;
 
     public GetFileCommand(
         IFileRepository repository,
         IFileInfoMapper mapper,
-        IProjectService projectService)
+        IProjectService projectService,
+        IAccessValidator accessValidator)
     {
       _repository = repository;
       _mapper = mapper;
       _projectService = projectService;
+      _accessValidator = accessValidator;
     }
 
     public async Task<OperationResultResponse<List<FileInfo>>> Execute(List<Guid> filesIds)
@@ -37,8 +42,14 @@ namespace LT.DigitalOffice.FileService.Business.Commands.File
 
       OperationResultResponse<List<FileInfo>> response = new();
 
+
+      if (!await _accessValidator.HasRightsAsync(Rights.AddEditRemoveProjects))
+      {
+        filesIds = await _projectService.CheckFilesAsync(filesIds, response.Errors);
+      }
+
       response.Body = (await _repository.GetAsync(
-        await _projectService.CheckFilesAsync(filesIds, response.Errors)))?.Select(x => _mapper.Map(x)).ToList();
+       filesIds))?.Select(x => _mapper.Map(x)).ToList();
       response.Status = response.Errors.Any() ? OperationResultStatusType.PartialSuccess : OperationResultStatusType.FullSuccess;
 
       return response;
