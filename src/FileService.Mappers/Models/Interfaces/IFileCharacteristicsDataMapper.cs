251ab90293e27e87917336b449fabc9297ba2357
VLiamin <43515136+VLiamin@users.noreply.github.com>
2022-08-05 01:19:13 +0300

    fixed bug (#50)

---
+++ src/FileService.Mappers/Models/Interfaces/IFileCharacteristicsDataMapper.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using LT.DigitalOffice.Kernel.Attributes;
+using LT.DigitalOffice.Models.Broker.Models.File;
+
+namespace LT.DigitalOffice.FileService.Mappers.Models.Interfaces
+{
+  [AutoInject]
+  public interface IFileCharacteristicsDataMapper
+  {
+    FileCharacteristicsData Map(Guid id, string name, string extension, long size, DateTime createdAtUtc);
+  }
+}
