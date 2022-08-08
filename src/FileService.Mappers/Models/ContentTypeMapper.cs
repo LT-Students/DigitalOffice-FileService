ff77e4f15e58d5828295a678fbc7601df4561531
Vladimir Liamin <LyaminVA@yandex.ru>
2022-08-08 15:41:49 +0300

    removed variable

--- src/FileService.Mappers/Models/ContentTypeMapper.cs
+++ src/FileService.Mappers/Models/ContentTypeMapper.cs
@@ -1,18 +1,10 @@
 ﻿using LT.DigitalOffice.FileService.Mappers.Models.Interfaces;
-using Microsoft.AspNetCore.StaticFiles;
 using MimeTypes;
 
 namespace LT.DigitalOffice.FileService.Mappers.Models
 {
   public class ContentTypeMapper : IContentTypeMapper
   {
-    private readonly FileExtensionContentTypeProvider _fileExtensionContentTypeProvider;
-
-    public ContentTypeMapper()
-    {
-      _fileExtensionContentTypeProvider =  new FileExtensionContentTypeProvider();
-    }
-
     public string Map(string extension)
     {
       if (extension is null)
